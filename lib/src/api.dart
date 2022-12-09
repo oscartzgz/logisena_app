442ce8e4a457fe970360fe9dbc611165e5ab0c67
Oscar Tellez <oscartzgz@gmail.com>
2022-12-09 00:35:16 -0600

    change url connection and version

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static String domain = "http://sys.logisena.com/";
+  static String domain = "http://sys.logisena.com:1061/";
   // static String domain = "http://10.0.3.4:3002";
   static String apiUrl = "$domain/api/v1";
   // static final forgotPasswordUrl = "http://localhost:3002/password/new";
